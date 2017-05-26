a9bd9159f5e99493900f569c05be3fb83580413f
Reteoteigam <Reteoteigam@gmail.com>
2017-05-26 22:52:14 +0200

    create first view, add textfield and button, add method to button, fill method with intend logic

--- app/src/main/java/com/reteoteigam/workclock/MainActivity.java
+++ app/src/main/java/com/reteoteigam/workclock/MainActivity.java
@@ -1,14 +1,29 @@
 package com.reteoteigam.workclock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO Hello World in App?
+
+    }
+
+
+    public void sendMessage(View View) {
+//todo something
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
