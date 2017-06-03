78af9f992c08acc4b3403f37391dc5950ca76122
Maximilian Tunger <maximilian.tunger@gmail.com>
2017-06-03 16:19:42 +0200

    logger extended

--- app/src/main/java/com/reteoteigam/workclock/logic/utils/Logger.java
+++ app/src/main/java/com/reteoteigam/workclock/logic/utils/Logger.java
@@ -2,16 +2,198 @@ package com.reteoteigam.workclock.logic.utils;
 
 import android.util.Log;
 
-/**
- * Created by Sammy on 29.05.2017.
- */
-
 public class Logger {
-    public static void i(Class<?> clazz, String message) {
-        Log.i(clazz.getSimpleName(), message);
+    private static boolean isUseCompatible;
+
+    public static void init(boolean isOldCompatible) {
+        setOldCompatible(isOldCompatible);
     }
 
+    public static void setOldCompatible(boolean useComaptible) {
+        isUseCompatible = useComaptible;
+    }
+
+    private static String getTagName(String tagName) {
+        if (isUseCompatible && tagName.length() > 23) {
+            return tagName.substring(0, Math.min(23, tagName.length()));
+        }
+        return tagName;
+    }
+
+    private static String getTagName(Class<?> clazz) {
+        String simpleClassName = clazz.getSimpleName();
+        return getTagName(simpleClassName);
+    }
+
+    /**
+     * info logger
+     *
+     * @param clazz   the class to log
+     * @param message the message to log
+     */
+    public static void i(Class<?> clazz, String message) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.INFO)) {
+            Log.i(tagName, message);
+        }
+    }
+
+    /**
+     * info logger
+     *
+     * @param clazz     the class to log
+     * @param message   the message to log
+     * @param throwable the throwable to log
+     */
+    public static void i(Class<?> clazz, String message, Throwable throwable) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.INFO)) {
+            Log.i(tagName, message, throwable);
+        }
+    }
+
+    /**
+     * error logger
+     *
+     * @param clazz   the class to log
+     * @param message the message to log
+     */
+    public static void e(Class<?> clazz, String message) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.ERROR)) {
+            Log.e(tagName, message);
+        }
+    }
+
+    /**
+     * error logger
+     *
+     * @param clazz     the class to log
+     * @param message   the message to log
+     * @param throwable the throwable to log
+     */
     public static void e(Class<?> clazz, String message, Throwable throwable) {
-        Log.e(clazz.getSimpleName(), message, throwable);
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.ERROR)) {
+            Log.e(tagName, message, throwable);
+        }
+    }
+
+    /**
+     * debug logger
+     *
+     * @param clazz   the class to log
+     * @param message the message to log
+     */
+    public static void d(Class<?> clazz, String message) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.DEBUG)) {
+            Log.d(tagName, message);
+        }
+    }
+
+    /**
+     * debug logger
+     *
+     * @param clazz     the class to log
+     * @param message   the message to log
+     * @param throwable the throwable to log
+     */
+    public static void d(Class<?> clazz, String message, Throwable throwable) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.DEBUG)) {
+            Log.d(tagName, message, throwable);
+        }
+    }
+
+    /**
+     * verbose logger
+     *
+     * @param clazz   the class to log
+     * @param message the message to log
+     */
+    public static void v(Class<?> clazz, String message) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.VERBOSE)) {
+            Log.v(tagName, message);
+        }
+    }
+
+    /**
+     * verbose logger
+     *
+     * @param clazz     the class to log
+     * @param message   the message to log
+     * @param throwable the throwable to log
+     */
+    public static void v(Class<?> clazz, String message, Throwable throwable) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.VERBOSE)) {
+            Log.v(tagName, message, throwable);
+        }
+    }
+
+    /**
+     * warning logger
+     *
+     * @param clazz   the class to log
+     * @param message the message to log
+     */
+    public static void w(Class<?> clazz, String message) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.WARN)) {
+            Log.w(tagName, message);
+        }
+    }
+
+    /**
+     * warning logger
+     *
+     * @param clazz     the class to log
+     * @param message   the message to log
+     * @param throwable the throwable to log
+     */
+    public static void w(Class<?> clazz, String message, Throwable throwable) {
+        String tagName = getTagName(clazz);
+
+        if (Log.isLoggable(tagName, Log.WARN)) {
+            Log.w(tagName, message, throwable);
+        }
+    }
+
+    /**
+     * wtf logger
+     *
+     * @param clazz   the class to log
+     * @param message the message to log
+     */
+    public static void wtf(Class<?> clazz, String message) {
+        String tagName = getTagName(clazz);
+
+        Log.wtf(tagName, message);
+    }
+
+    /**
+     * wtf logger
+     *
+     * @param clazz     the class to log
+     * @param message   the message to log
+     * @param throwable the throwable to log
+     */
+    public static void wtf(Class<?> clazz, String message, Throwable throwable) {
+        String tagName = getTagName(clazz);
+
+        Log.wtf(tagName, message, throwable);
     }
 }
+
+
